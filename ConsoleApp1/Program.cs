77d23fb0889e6a31258efde82eda7fad5643a7b2
Дмитрий <Sitnikov333@Rambler.ru>
2019-09-13 00:56:46 +0500

    New app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculate
+{
+    public class Calculator
+    {
+        public static void Sum(int a, int b) => Write.WriteNum(a + b);
+
+        public static void Del(int a, int b) => Write.WriteNum(a - b);
+        
+        public static void Mult(int a, int b) => Write.WriteNum(a * b);
+
+        public static void Div(int a, double b) => Write.WriteNum(a / b);
+    }
+
+    public class Write
+    {
+        public static void WriteNum(double c) => Console.WriteLine(Convert.ToString(c));
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //К примеру
+            Calculator.Div(5,2);
+            Console.ReadKey();
+        }
+    }
+}
